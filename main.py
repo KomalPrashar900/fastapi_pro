89f744d865476af73cb538bc12c5c6b50a4641dc
KomalPrashar900 <prasharkomal57@gmail.com>
2026-03-17 10:22:03 +0530

    update

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ models.Base.metadata.create_all(bind = engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
